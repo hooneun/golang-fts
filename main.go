f3567cf6239ca5e67a0b2abe4406bc1d28ca7271
khon@toun28.com <4djrdmfqjfRjdi!>
2021-04-06 11:57:40 +0900

    default string search

--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"encoding/xml"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type documents struct {
+	Documents []document `xml:"doc"`
+}
+
+type document struct {
+	Title string `xml:"title"`
+	URL   string `xml:"url"`
+	Text  string `xml:"abstract"`
+	ID    int
+}
+
+func loadDocuments(path string) ([]document, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	dec := xml.NewDecoder(f)
+	var dump documents
+	if err := dec.Decode(&dump); err != nil {
+		return nil, err
+	}
+
+	docs := dump.Documents
+	for i := range docs {
+		docs[i].ID = i
+	}
+	return docs, nil
+}
+
+func search(docs documents, term string) documents {
+	var r documents
+	for _, doc := range docs.Documents {
+		if strings.Contains(doc.Text, term) {
+			r.Documents = append(r.Documents, doc)
+		}
+	}
+	return r
+}
 
 func main() {
-	fmt.Println("vim-go")
+	doc, err := loadDocuments("./enwiki-latest-abstract1.xml")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(doc[0])
+
 }
